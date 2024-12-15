a697c3a9fe14ff47ce46d505707b6da95d32c306
tkgka <ksh17171@gmail.com>
2024-12-15 18:41:44 +0900

    ./

--- FediverseClient/Projects/Feature/UIComponent/Sources/PopUpSheet/PopUpSheet.swift
+++ FediverseClient/Projects/Feature/UIComponent/Sources/PopUpSheet/PopUpSheet.swift
@@ -25,20 +25,56 @@ extension View {
                             isPresented.wrappedValue = false
                         }
                     }
-                VStack {
-                    Spacer()
-                    content()
-                        .frame(maxWidth: .infinity)
-                        .padding(.bottom, 32)
-                        .background {
-                            RoundedRectangle(cornerRadius: cornerRadius)
-                                .fill(backgroundColor)
-                        }
-                        .padding(.horizontal, 16)
-                }
-                .transition(.move(edge: .bottom))
+                PopUPSheetView(
+                    cornerRadius: cornerRadius,
+                    backgroundColor: backgroundColor,
+                    isPresented: isPresented,
+                    content: content
+                )
             }
         }
         .animation(.easeInOut, value: isPresented.wrappedValue)
+    }
+}
+
+
+struct PopUPSheetView<PopUPContent: View>: View {
+        
+    let cornerRadius: CGFloat
+    let backgroundColor: Color
+    @Binding var isPresented: Bool
+    
+    @State var offset: CGFloat = 0
+    @ViewBuilder var content: () -> PopUPContent
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            content()
+                .frame(maxWidth: .infinity)
+                .padding(.bottom, 32)
+                .background {
+                    RoundedRectangle(cornerRadius: cornerRadius)
+                        .fill(backgroundColor)
+                }
+                .padding(.horizontal, 16)
+        }
+        .offset(y: offset)
+        .gesture(
+            DragGesture()
+                .onChanged { value in
+                    let offsetValue = max(-20, value.translation.height)
+                    offset = offsetValue
+                }
+                .onEnded { value in
+                    guard offset > 200 else {
+                        offset = 0
+                        return
+                    }
+                    isPresented = false
+                }
+        )
+        .animation(.bouncy, value: offset)
+        .transition(.move(edge: .bottom))
     }
 }
