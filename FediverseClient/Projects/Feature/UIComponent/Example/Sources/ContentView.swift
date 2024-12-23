fccb9a0862601caaa778d729531405598b43f55f
tkgka <ksh17171@gmail.com>
2024-12-23 23:52:49 +0900

    ./

--- FediverseClient/Projects/Feature/UIComponent/Example/Sources/ContentView.swift
+++ FediverseClient/Projects/Feature/UIComponent/Example/Sources/ContentView.swift
@@ -4,25 +4,81 @@ import UIComponent
 public struct ContentView: View {
     public init() {}
     
+    @State var selectedTab = ""
+    
     public var body: some View {
         NavigationView {
-            ScrollView {
-                LazyVStack {
-                    ForEach(Views.allCases, id: \.self) { viewContent in
-                        NavigationLink {
-                            viewContent.view
-                        } label: {
-                            RoundedRectangle(cornerRadius: 16)
-                                .fill(.blue)
-                                .overlay {
-                                    Text(viewContent.rawValue)
-                                        .foregroundStyle(.white)
-                                }
-                                .frame(height: 56)
-                                .padding(.horizontal, 16)
-                        }
-                        .buttonStyle(ShrinkButtonStyle())
+            VStack(spacing: 0) {
+                TabView(selection: $selectedTab) {
+                    main
+                        .toolbar(.hidden, for: .tabBar)
+                        .tag("1")
+                    Text("!@#")
+                        .toolbar(.hidden, for: .tabBar)
+                        .tag("2")
+                    Text("123")
+                        .toolbar(.hidden, for: .tabBar)
+                        .tag("3")
+                }
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        selectedTab = "1"
+                    } label: {
+                        Image("star")
+                            .resizable()
+                            .scaledToFit()
                     }
+                    
+                    Spacer()
+                    
+                    Button {
+                        selectedTab = "2"
+                    } label: {
+                        Image("stack")
+                            .resizable()
+                            .scaledToFit()
+                    }
+                    
+                    
+                    Spacer()
+                    
+                    Button {
+                        selectedTab = "3"
+                    } label: {
+                        Image("setting")
+                            .resizable()
+                            .scaledToFit()
+                    }
+                    
+                    Spacer()
+                }
+                .padding(.top, 10)
+                .frame(height: 50)
+                .background(.regularMaterial)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private var main: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(Views.allCases, id: \.self) { viewContent in
+                    NavigationLink {
+                        viewContent.view
+                    } label: {
+                        RoundedRectangle(cornerRadius: 16)
+                            .fill(.blue)
+                            .overlay {
+                                Text(viewContent.rawValue)
+                                    .foregroundStyle(.white)
+                            }
+                            .frame(height: 56)
+                            .padding(.horizontal, 16)
+                    }
+                    .buttonStyle(ShrinkButtonStyle())
                 }
             }
         }
